1276822d85f01e53a338ab815d8dfecf25de3d4c
Siim Leosk <siim.leosk@hotmail.com>
2023-03-07 07:39:11 +0200

    Submit button work

--- src/main/java/com/example/mydiscgolfbag/AppController.java
+++ src/main/java/com/example/mydiscgolfbag/AppController.java
@@ -1,14 +1,31 @@
 package com.example.mydiscgolfbag;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class AppController {
-	@FXML
-	private Label welcomeText;
 
 	@FXML
-	protected void onHelloButtonClick() {
-		welcomeText.setText("This button works!");
+	private Button SubmitBtn;
+
+	@FXML
+	private TextField txtField1;
+
+	@FXML
+	private Label confirmText;
+
+	@FXML
+	void onSubmitButtonClick(ActionEvent event) {
+		System.out.println("Hi");
+		confirmText.setText("Disc added.");
 	}
-}
+
+	@FXML
+	void sayConfirmClick(ActionEvent event) {
+
+	}
+	}
--- src/main/java/com/example/mydiscgolfbag/MyDiscgolfBagApp.java
+++ src/main/java/com/example/mydiscgolfbag/MyDiscgolfBagApp.java
@@ -11,7 +11,7 @@ public class MyDiscgolfBagApp extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(MyDiscgolfBagApp.class.getResource("BagMain-View.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 420, 340);
+		Scene scene = new Scene(fxmlLoader.load(), 600, 400);
 		stage.setTitle("MyDiscgolfBag Applicaton ");
 		stage.setScene(scene);
 		stage.show();
