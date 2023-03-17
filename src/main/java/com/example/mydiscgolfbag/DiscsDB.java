7ccd5f5d7b2746f06e2d86169f31ed469f355075
Siim Leosk <siim.leosk@hotmail.com>
2023-03-17 18:58:40 +0200

    JavaFX changes

--- src/main/java/com/example/mydiscgolfbag/DiscsDB.java
+++ src/main/java/com/example/mydiscgolfbag/DiscsDB.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Message {
+public class Discs {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
